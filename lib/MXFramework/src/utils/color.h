97e1b169e1183614092bc4c49c2872f9b196d089
Henry Li <henry1943@163.com>
2025-01-03 09:51:38 +0800

    feat: use lv_color_hex

--- lib/MXFramework/src/utils/color.h
+++ lib/MXFramework/src/utils/color.h
@@ -1,9 +1,9 @@
+#pragma once
+
 #include <lvgl.h>
 
 inline lv_color_t rgb(uint8_t r, uint8_t g, uint8_t b) {
   return lv_color_make(r, g, b);
 }
 
-inline lv_color_t rgb(uint32_t rgb) {
-  return lv_color_make((rgb >> 16) & 0xFF, (rgb >> 8) & 0xFF, rgb & 0xFF);
-}
+inline lv_color_t rgb(uint32_t rgb) { return lv_color_hex(rgb); }
