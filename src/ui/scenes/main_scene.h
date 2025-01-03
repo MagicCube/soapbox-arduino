8d7401a158d5c158755d6e5648daa844e93297bf
Henry Li <henry1943@163.com>
2025-01-03 21:48:06 +0800

    feat: add transform

--- src/ui/scenes/main_scene.h
+++ src/ui/scenes/main_scene.h
@@ -6,36 +6,64 @@
 
 LV_IMAGE_DECLARE(microphone);
 
+#define BUTTON_COLOR rgb(0xFF2C55)
+#define BG_COLOR rgb(0x522069)
+
+static lv_style_t bigRingPressedStyle;
+static lv_style_t midRingPressedStyle;
+
 class MainScene : public MXScene {
  protected:
   MXObject* speakButton;
+  MXObject* bigRing;
+  MXObject* midRing;
 
   inline void onInit() override {
     MXScene::onInit();
-    root()->bg(rgb(0x522069)).scrollable(false);
-    speakButton =
-        &root()
-             ->button()
-             .size(256)
-             .center(0, 140)
-             .bg(rgb(0xFF2C55))
-             .rounded_full()
-             .onClick([](MXEvent* e) { Serial.println("Clicked"); })
-             .onPressed([](MXEvent* e) { Serial.println("Pressed"); })
-             .onReleased([](MXEvent* e) { Serial.println("Released"); });
-    speakButton->object()
-        .size(200)
-        .center()
-        .bg_white(0.1)
-        .rounded_full()
-        .clickable(false);
-    speakButton->object()
-        .size(144)
-        .center()
-        .bg_white(0.1)
-        .rounded_full()
-        .clickable(false);
-    speakButton->image(&microphone).center(0, -6).clickable(false);
+    root()->bg(BG_COLOR).scrollable(false);
+
+    bigRing = &root()
+                   ->object()
+                   .size(256)
+                   .center(0, 96)
+                   .bg_white(0.1)
+                   .rounded_full()
+                   .clickable(false);
+    midRing = &bigRing->object()
+                   .size(200)
+                   .center()
+                   .bg_white(0.1)
+                   .rounded_full()
+                   .clickable(false);
+    speakButton = &bigRing->button()
+                       .size(144)
+                       .center()
+                       .bg(BUTTON_COLOR)
+                       .rounded_full()
+                       .onClick([](MXEvent* e) { Serial.println("Clicked"); })
+                       .onPressed([this](MXEvent* e) {
+                         bigRing->add_style(&bigRingPressedStyle);
+                         bigRing->bg_opacity(0.05);
+                         midRing->add_style(&midRingPressedStyle);
+                         midRing->bg_opacity(0.05);
+                       })
+                       .onReleased([this](MXEvent* e) {
+                         bigRing->remove_style(&bigRingPressedStyle);
+                         bigRing->bg_opacity(0.1);
+                         midRing->remove_style(&midRingPressedStyle);
+                         midRing->bg_opacity(0.1);
+                       });
+    speakButton->image(&microphone).center(0).clickable(false);
+
+    // Big ring pressed style
+    init_style(&bigRingPressedStyle, 40);
+    init_style(&midRingPressedStyle, 16);
+  }
+
+  inline void init_style(lv_style_t* style, int32_t size) {
+    lv_style_init(style);
+    lv_style_set_transform_width(style, size);
+    lv_style_set_transform_height(style, size);
   }
 
   inline void dispose() override { MXScene::dispose(); }
