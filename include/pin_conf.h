89c577a3f41c2278227a1a8005671f2d625c23b0
Henry Li <henry1943@163.com>
2025-01-04 22:45:13 +0800

    refactor: rename

--- include/pin_conf.h
+++ include/pin_conf.h
@@ -10,10 +10,10 @@
 #define TFT_SDA2 13
 #define TFT_SDA3 14
 
-#define TOUCH_PIN_NUM_I2C_SCL 8
-#define TOUCH_PIN_NUM_I2C_SDA 7
-#define TOUCH_PIN_NUM_INT 41
-#define TOUCH_PIN_NUM_RST 40
+#define TOUCH_SCL 8
+#define TOUCH_SDA 7
+#define TOUCH_INT 41
+#define TOUCH_RST 40
 
 #define SD_MMC_D0 2
 #define SD_MMC_D1 1
--- src/drivers/touch_cst816s_driver.cpp
+++ src/drivers/touch_cst816s_driver.cpp
@@ -8,15 +8,13 @@
 #include "pin_conf.h"
 
 ESP_PanelTouch* touch_init() {
-  ESP_PanelBus_I2C* bus =
-      new ESP_PanelBus_I2C(TOUCH_PIN_NUM_I2C_SCL, TOUCH_PIN_NUM_I2C_SDA,
-                           ESP_LCD_TOUCH_IO_I2C_CST816S_CONFIG());
+  ESP_PanelBus_I2C* bus = new ESP_PanelBus_I2C(
+      TOUCH_SCL, TOUCH_SDA, ESP_LCD_TOUCH_IO_I2C_CST816S_CONFIG());
   bus->configI2cFreqHz(400000);
   bus->begin();
 
-  auto touch =
-      new ESP_PanelTouch_CST816S(bus, DISPLAY_RES_WIDTH, DISPLAY_RES_HEIGHT,
-                                 TOUCH_PIN_NUM_RST, TOUCH_PIN_NUM_INT);
+  auto touch = new ESP_PanelTouch_CST816S(
+      bus, DISPLAY_RES_WIDTH, DISPLAY_RES_HEIGHT, TOUCH_RST, TOUCH_INT);
   touch->init();
   touch->begin();
   return touch;
