cc9a83e2dd619632b8803a937377a24fe60e07c7
Henry Li <henry1943@163.com>
2025-01-05 21:33:02 +0800

    fix: bugfix

--- include/pin_conf.h
+++ include/pin_conf.h
@@ -1,5 +1,5 @@
-#if DEVICE_TYPE == kevin_s3_sp_v3
+#if DEVICE_TYPE_KEVIN_S3_SP_V3
 #include "devices/kevin_s3_sp_v3/pin_conf.h"
-#elif DEVICE_TYPE == jc3636w518c
+#elif DEVICE_TYPE_JC3636W518C
 #include "devices/jc3636w518c/pin_conf.h"
 #endif
