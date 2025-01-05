443aab79300f468f24bf54f286d24e5d2f8ca8b7
Henry Li <henry1943@163.com>
2025-01-05 22:04:54 +0800

    refactor: rename

--- include/pin_conf.h
+++ include/pin_conf.h
@@ -1,5 +1,7 @@
-#if DEVICE_TYPE_KEVIN_S3_SP_V3
+#ifdef DEVICE_KEVIN_S3_SP_V3
 #include "devices/kevin_s3_sp_v3/pin_conf.h"
-#elif DEVICE_TYPE_JC3636W518C
+#endif
+
+#ifdef DEVICE_JC3636W518C
 #include "devices/jc3636w518c/pin_conf.h"
 #endif
